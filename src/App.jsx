d244ca0395ce3cf273a43ddd732e2fb306266033
mariem <mariamkhaled709@gmail.com>
2023-09-21 13:29:39 +0300

    first deployment

---
+++ src/App.jsx
@@ -0,0 +1,111 @@
+import { useState,useEffect ,useRef} from 'react';
+import Questions from './components/Qusestions';
+import './App.css'
+import { nanoid } from 'nanoid';
+import blobDark from './assets/blobanimation.svg'
+import blobLg from './assets/blobanimation-lg.svg'
+
+
+
+
+function App() {
+ const[started,setStarted]=useState(false);
+ const[IsCorrect,setIsCorrect]=useState(0);
+ const[IsChecked,setIsChecked]=useState(false);
+ const[questions,setQuestions]=useState([]);
+ const count=useRef(0);
+ const shuffleArr=(arr)=>arr.sort(()=>Math.random()-0.5);
+  useEffect(()=>{
+      (async  ()=> {
+        const res= await fetch('https://opentdb.com/api.php?amount=6');
+        const data=await res.json();
+        const q=[];
+        data.results.forEach(question=>q.push({
+          id:nanoid(),
+          question:question.question,
+          answers:shuffleArr([...question.incorrect_answers,question.correct_answer]), 
+          checked:false,
+          selected:null,
+          correct:question.correct_answer,
+        }
+        ))
+        setQuestions(q)
+      })(); 
+    },[count.current]);
+
+    function handleClicks (answer,id){
+    setQuestions(questions=>questions.map(q=>{
+    if (id===q.id) {
+      return {...q,selected:answer };}
+      else return q;
+    }
+      ))
+    }
+   
+  
+   function Check( ) 
+      {   setIsChecked(true); 
+          let correct=0
+          questions.forEach(q=>{
+          if (q.selected===null) return})
+          setQuestions(questions=>questions.map(q=>{return{...q,checked:true}}))  
+          questions.forEach(q=>{
+          if (q.correct===q.selected) {
+          ;
+          correct+=1;
+         
+           
+      } 
+      else  return q
+    }
+      ) 
+      setIsCorrect(correct);
+   }
+
+   function tryAgain() {
+    if (IsChecked) {
+      setIsCorrect(0)
+      setIsChecked(false)
+      ++count.current;
+    }
+   }
+
+ const questionEl=questions.map(question=>{
+ return(<Questions 
+      started={started} 
+      key={question.id}  
+      id={question.id}
+      question={question}
+      handleClicks={handleClicks} 
+      />) 
+    }) 
+   return (
+      <>
+       <main className='container' >
+          <div className="blob ">
+            <img src={blobDark} alt="blob-animation" className='blob-dark'/>
+           <img src={blobLg} alt="blob-animation" className='blob-lg' /> 
+           </div>
+        {!started ?
+        <section  className="col started" aria-label='Started'>
+          <h1 className='title'>Quizzical</h1> 
+          <button className='btn' onClick={()=>setStarted(!started)}>Start</button>
+        </section>
+          :
+           <section className="col">
+            {questionEl}
+            <br />
+            <div className="container">
+            {IsChecked?`correct answer ${IsCorrect}/${questions.length}`:""}
+            </div>
+            <br />
+           <button className='btn' onClick={IsChecked?tryAgain:Check} >{IsChecked?'Try Again':'Check'}</button>
+           </section>   }
+           
+           </main>
+          
+      </>
+      ) 
+    }
+export default App
+
---
+++ src/components/Qusestions.jsx
@@ -0,0 +1,44 @@
+import { nanoid } from "nanoid";
+import React from "react";
+
+
+
+ export default function Questions(props) {
+function handleAnswer(answer) {
+    if (props.question.checked) return;
+  props.handleClicks(answer,props.id);
+ 
+}    
+ let answers=props.question.answers.map((answer)=>{
+       let id='';
+       if (props.question.checked) {          
+        if (props.question.correct===answer) 
+           { id='correct'; }    
+    else if (props.question.selected===answer) 
+        { id='incorrect';}
+        else  id="not-selected";
+              
+    }    
+    return(
+       <button key={nanoid()} 
+       onClick={()=>handleAnswer(answer)}  
+       id={id} 
+       className="answers"
+       >{answer.replace(/&quot;|&#039;|&iuml;|&amp;|&deg;/g,"")}</button>
+    )
+
+})
+
+    return(     
+    <div className="container" >
+        <div className="col">
+        <h2>{props.question.question.replace(/&quot;|&#039;/g,"").replace(/&eacute;/,'e')}</h2>
+        <br></br>
+                    <div>
+                    {answers}
+                    </div>
+        </div>
+    </div>
+        
+    )
+}
